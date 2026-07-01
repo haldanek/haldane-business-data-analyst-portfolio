0826e52abcdb7571c3a39da4431b0646a8dd5637
haldanek <khaldane244@gmail.com>
2026-07-01 16:29:17 -0400

    feat: add portfolio styling

--- haldane-business-data-analyst-portfolio/src/pages/About.tsx
+++ haldane-business-data-analyst-portfolio/src/pages/About.tsx
@@ -1,7 +1,19 @@
 function About() {
   return (
     <main>
-      <h1>About Me</h1>
+      <div className='page-card'>
+        <h1>About Me</h1>
+
+        <p>
+          I am transitioning from education, instructional design, and technical
+          writing into business data analytics.
+        </p>
+
+        <p>
+          My background has strengthened my ability to analyze information,
+          solve problems, and communicate insights clearly to stakeholders.
+        </p>
+      </div>
     </main>
   );
 }
--- haldane-business-data-analyst-portfolio/src/pages/Home.tsx
+++ haldane-business-data-analyst-portfolio/src/pages/Home.tsx
@@ -1,14 +1,45 @@
 function Home() {
   return (
     <main>
-      <h1>Kimberlee Haldane</h1>
+      <section className='hero'>
+        <h1>Kimberlee Haldane</h1>
 
-      <h2>Business Data Analyst Portfolio</h2>
+        <h2>Business Data Analyst Portfolio</h2>
 
-      <p>
-        Transforming data into actionable business insights through analysis,
-        visualization, and communication.
-      </p>
+        <p>
+          Transforming data into actionable business insights through analysis,
+          visualization, and communication.
+        </p>
+
+        <div className='hero-buttons'>
+          <button>View Projects</button>
+          <button>Contact Me</button>
+        </div>
+      </section>
+      
+      
+
+      <section className='stats-section'>
+        <div className='stat-card'>
+          <h3>Projects</h3>
+          <p>8</p>
+        </div>
+
+        <div className='stat-card'>
+          <h3>Dashboards</h3>
+          <p>3</p>
+        </div>
+
+        <div className='stat-card'>
+          <h3>Datasets</h3>
+          <p>10+</p>
+        </div>
+
+        <div className='stat-card'>
+          <h3>Skills</h3>
+          <p>8</p>
+        </div>
+      </section>
     </main>
   );
 }
